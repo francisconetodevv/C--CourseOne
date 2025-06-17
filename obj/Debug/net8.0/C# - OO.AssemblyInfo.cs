704b2c89a01b1f17597f93a96ec2072473709dfa
Francisco <franciscoocontatoo@gmail.com>
2025-06-17 20:43:39 -0300

    att projeto

--- obj/Debug/net8.0/C# - OO.AssemblyInfo.cs
+++ obj/Debug/net8.0/C# - OO.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C# - OO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f5f06c0b81aca3c423a61efb45bbf35db91a12")]
 [assembly: System.Reflection.AssemblyProductAttribute("C# - OO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C# - OO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
